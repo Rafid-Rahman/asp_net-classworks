d89d3c55e8ad31031c8d30e14dbe724753e19fcf
Rafid <59876113+Rafid-Rahman@users.noreply.github.com>
2023-10-01 16:16:56 +0600

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RegistrationForm.Models;
+
+namespace RegistrationForm.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+        /*
+        [HttpPost]
+        public ActionResult Registration(FormCollection fc)
+        {
+            ViewBag.Uname = fc["Username"];
+            ViewBag.Upass = fc["Password"];
+            ViewBag.Uid = fc["Id"];
+            ViewBag.Ugender = fc["Gender"];
+            ViewBag.Uprofession = fc["Profession"];
+            ViewBag.Uhobbies = fc["Hobbies"];
+            return View();
+        }
+        */
+        [HttpPost]
+        public ActionResult Registration(Registration m)
+        {
+            return View(m);
+        }
+    }
+}
+ 
---
+++ Registration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RegistrationForm.Models
+{
+    public class Registration
+    {
+
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public int Id { get; set; }
+        public string Gender { get; set; }
+        public string Profession { get; set; }
+        public string Hobbies { get; set; }
+
+    }
+}
