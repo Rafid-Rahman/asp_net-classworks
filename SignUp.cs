69d734f511505aa480ce5308c3ea619a72cd3be5
Rafid <59876113+Rafid-Rahman@users.noreply.github.com>
2023-10-08 15:50:35 +0600

    Add files via upload

---
+++ SignUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AB_Task_1.Models
+{
+    public class SignUp
+    {
+        [Required(ErrorMessage = "Provide Your Name")]
+        [RegularExpression("^[a-zA-Z]{4,50}$", ErrorMessage= "Name should be between 4 to 50 characters and no special characters.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage ="Provide Your User ID")]
+        [RegularExpression("^[a-zA-Z0-9\\-_]{4,12}$", ErrorMessage = "User ID should be between 4 to 12 characters and only numbers, characters are allowed.")]
+        public string UserId { get; set; }
+
+
+        public string Password { get; set; }
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public string Dob { get; set; }
+
+    }
+}
---
+++ SignUpController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AB_Task_1.Models;
+
+namespace AB_Task_1.Controllers
+{
+    public class SignUpController : Controller
+    {
+        // GET: SignUp
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SignUp(SignUp s)
+        {
+            return View(s);
+        }
+    }
+}
